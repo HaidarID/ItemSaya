d51761b31fafd50d16f9020d3482baef4d0253f0
HaidarID <56497094+HaidarID@users.noreply.github.com>
2020-01-27 01:00:07 +0700

    This is example

---
+++ src/main/java/com/haidarid/itemsaya/items/IronSword.java
@@ -0,0 +1,31 @@
+package com.haidarid.itemsaya.items;
+
+import com.haidarid.itemsaya.Item;
+import com.haidarid.itemsaya.Spell;
+
+import java.util.List;
+
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+
+public final class IronSword extends Item {	
+	public IronSword() {
+         //material, rarity, name, Displayname, damage, Has skill  
+		super(Material.IRON_SWORD, 5, "ironsword", "Iron_Sword", 5, true);
+	}
+
+	@Override
+	public boolean onSpellComplete(Player player, Spell spell1, Spell spell2, Spell spell3) {
+		if (spell1 == Spell.Left && spell2 == Spell.Left && spell3 == Spell.Left) {
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public List<String> setLore() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
