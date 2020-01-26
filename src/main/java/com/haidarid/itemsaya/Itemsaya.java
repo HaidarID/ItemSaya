a6b3bb9f627b9e0f971e01372a3126ed301ba71d
HaidarID <56497094+HaidarID@users.noreply.github.com>
2020-01-27 00:19:38 +0700

    Create Itemsaya.java
    main class

---
+++ src/main/java/com/haidarid/itemsaya/Itemsaya.java
@@ -0,0 +1,37 @@
+package com.haidarid.itemsaya;
+
+import com.haidarid.itemsaya.command.Cmd;
+import com.haidarid.itemsaya.listener.SpellReader;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Itemsaya extends JavaPlugin {
+	
+	private static Itemsaya Instance;
+	private ItemManager ItemManager;
+
+	
+	@Override
+	public void onEnable() {
+		try {
+      Instance = this;
+      ItemManager = new ItemManager();
+      ItemManager.registerItems();
+      Bukkit.getPluginManager().registerEvents(new SpellReader(), this);
+      getCommand("itemsaya").setExecutor(new Cmd());
+      getConfig().options().copyDefaults();
+  		saveDefaultConfig();
+		} catch (Exception exc) {
+			// TODO: handle exception
+		}
+	}
+	
+	@Override
+	public void onDisable() {
+	}
+
+	public static Itemsaya getInstance() {
+		return Instance;
+	}
+}
